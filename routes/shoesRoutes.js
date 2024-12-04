882b217d63b345555dc4592ac86b21ea2c4fb5c5
angx7 <angelmirocu@gmail.com>
2024-12-04 14:08:01 -0600

    finish

--- routes/shoesRoutes.js
+++ routes/shoesRoutes.js
@@ -99,4 +99,83 @@ router.get('/trends', async (req, res) => {
   }
 });
 
+// Ruta para obtener las novendades
+router.get('/new', async (req, res) => {
+  try {
+    const newShoes = await Shoe.find().sort({ uploadDate: -1 }).limit(4);
+    // Mapea las imágenes a base64 para facilitar su uso en el front
+    const formattedNewShoes = newShoes.map((shoe) => ({
+      _id: shoe._id,
+      shoeId: shoe.shoeId,
+      description: shoe.description,
+      category: shoe.category,
+      uploadDate: shoe.uploadDate,
+      sales: shoe.sales,
+      image: `data:${shoe.image.contentType};base64,${shoe.image.data.toString(
+        'base64',
+      )}`,
+    }));
+    res.status(200).json(formattedNewShoes);
+  } catch (err) {
+    res.status(500).send('Error al obtener las novedades: ' + err.message);
+  }
+});
+
+// ruta para obtener el modelo más vendido por categoría
+router.get('/top-category', async (req, res) => {
+  try {
+    const categories = ['casual', 'senora', 'enfermera', 'joven', 'escolar'];
+    const topShoes = await Promise.all(
+      categories.map(async (category) => {
+        const topShoe = await Shoe.findOne({ category }).sort({ sales: -1 });
+        if (topShoe) {
+          return {
+            category,
+            shoe: {
+              _id: topShoe._id,
+              shoeId: topShoe.shoeId,
+              description: topShoe.description,
+              uploadDate: topShoe.uploadDate,
+              sales: topShoe.sales,
+              image: `data:${topShoe.image.contentType};base64,${topShoe.image.data.toString('base64')}`,
+            },
+          };
+        } else {
+          return { category, shoe: null };
+        }
+      }),
+    );
+    res.status(200).json(topShoes);
+  } catch (err) {
+    res
+      .status(500)
+      .send(
+        'Error al obtener el modelo más vendido por categoría: ' + err.message,
+      );
+  }
+});
+
+// Ruta para obtener todos los zapatos de una categoría
+router.get('/:category', async (req, res) => {
+  try {
+    const category = req.params.category;
+    const shoes = await Shoe.find({ category });
+    // Mapea las imágenes a base64 para facilitar su uso en el front
+    const formattedShoes = shoes.map((shoe) => ({
+      _id: shoe._id,
+      shoeId: shoe.shoeId,
+      description: shoe.description,
+      category: shoe.category,
+      uploadDate: shoe.uploadDate,
+      sales: shoe.sales,
+      image: `data:${shoe.image.contentType};base64,${shoe.image.data.toString(
+        'base64',
+      )}`,
+    }));
+    res.status(200).json(formattedShoes);
+  } catch (err) {
+    res.status(500).send('Error al obtener los zapatos: ' + err.message);
+  }
+});
+
 module.exports = router;
